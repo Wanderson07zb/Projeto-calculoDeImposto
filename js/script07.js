f5a9ed405c7677b699309f9853279ae2ee39e170
Wanderson07zb <wanderson07zb@gmail.com>
2024-08-02 23:58:26 -0300

    Add files via upload

--- js/script07.js
+++ js/script07.js
@@ -21,7 +21,8 @@ function executarScriptJs() {
         let valorLiquidoAoMes;
         let valorLiquidoPorAno;
         // Mensagens adicional nas colunas da tabela
-        let imposto;
+        let impostoPorcentagem;
+        let impostoReal;
         let mensagem = "VALORES";
         // Parcelas a deduzir do IR
         let aliquotaDeSetePontoCincoPorCento = 169.44;
@@ -31,41 +32,48 @@ function executarScriptJs() {
 
         // Verificação de condições de dedução pelo IR
         if (valorBrutoPorMes <= 2259.20) {
-            imposto = 0;
+            impostoPorcentagem = 0;
+            impostoReal = 0;
             mensagem = "O(A) SENHOR(A) NÃO PAGA IMPOSTO DE RENDA"
             valorLiquidoAoMes = valorBrutoPorMes;
             valorLiquidoPorAno = valorBrutoAnual;
         }
         else if (valorBrutoPorMes <= 2828.65) {
-            imposto = 7.5;
+            impostoPorcentagem = 7.5;
+            impostoReal =  169.44;
             valorLiquidoAoMes = valorBrutoPorMes - aliquotaDeSetePontoCincoPorCento;
             valorLiquidoPorAno = valorLiquidoAoMes * 12;
         }
         else if (valorBrutoPorMes <= 3751.05) {
-            imposto = 15;
+            impostoPorcentagem = 15;
+            impostoReal =  381.44;
             valorLiquidoAoMes = valorBrutoPorMes - aliquotaDeQuinzePorCento;
             valorLiquidoPorAno = valorLiquidoAoMes * 12;
         }
         else if (valorBrutoPorMes <= 4664.68) {
-            imposto = 22.5;
+            impostoPorcentagem = 22.5;
+            impostoReal =  662.77;
             valorLiquidoAoMes = valorBrutoPorMes - aliquotaDeVinteEDoisPontoCincoPorCento;
             valorLiquidoPorAno = valorLiquidoAoMes * 12;
         }
         else if (valorBrutoPorMes > 4664.68) {
-            imposto = 27.5;
+            impostoPorcentagem = 27.5;
+            impostoReal = 896.00;
             valorLiquidoAoMes = valorBrutoPorMes - aliquotaDeVinteESetePontoCincoPorCento;
             valorLiquidoPorAno = valorLiquidoAoMes * 12;
         }
 
         // Chamar a função criarTabelaIR
-        criarTabelaIR(salarioMinimo, valorBrutoAnual, valorBrutoPorMes, valorLiquidoPorAno, valorLiquidoAoMes, imposto, mensagem)
+        criarTabelaIR(salarioMinimo, valorBrutoAnual, valorBrutoPorMes, valorLiquidoPorAno, valorLiquidoAoMes, impostoPorcentagem, impostoReal, mensagem)
 
 
     }
-    function criarTabelaIR(salarioMinimo, valorBrutoAnual, valorBrutoPorMes, valorLiquidoPorAno, valorLiquidoAoMes, imposto, mensagem) {
+
+    function criarTabelaIR(salarioMinimo, valorBrutoAnual, valorBrutoPorMes, valorLiquidoPorAno, valorLiquidoAoMes, impostoPorcentagem, impostoReal, mensagem) {
         
         // Substituir ponto por vírgula
-        let conversaoImposto = imposto.toFixed(2).toString().replace(".", ",");
+        let conversaoImpostoPorcentagem = impostoPorcentagem.toFixed(2).toString().replace(".", ",");
+        let conversaoImpostoReal = impostoReal.toFixed(2).toString().replace(".", ",");
         let conversaoSalarioMinimo = salarioMinimo.toFixed(2).toString().replace(".", ",");
         let conversaoValorBrutoAnual = valorBrutoAnual.toFixed(2).toString().replace(".", ",");
         let conversaoValorLiquidoPorAno = valorLiquidoPorAno.toFixed(2).toString().replace(".", ",");
@@ -76,8 +84,8 @@ function executarScriptJs() {
         '<table border="1" cellpadding="5" cellspacing="0">' +
            '<tr> <th id="detalhesTable" colspan="2">Detalhes do Imposto de Renda</th> </tr>' +
                 '<tr id="fundoColorBackground"> <td>CATEGORIA</td> <td>' + mensagem + ' </td> </tr>' +
-                '<tr> <td>IMPOSTO (%)</td> <td>' + conversaoImposto + '% </td> </tr>' +
-                '<tr> <td>IMPOSTO (R$)</td> <td>' + conversaoImposto + '</td> </tr>' +
+                '<tr> <td>IMPOSTO (%)</td> <td>' + conversaoImpostoPorcentagem + '% </td> </tr>' +
+                '<tr> <td>IMPOSTO (R$)</td> <td>' + conversaoImpostoReal + '</td> </tr>' +
                 '<tr> <td>SALÁRIO MÍNIMO (R$)</td> <td>' + conversaoSalarioMinimo + '</td> </tr>' +
                 '<tr> <td>VALOR BRUTO ANUAL (R$)</td> <td>' + conversaoValorBrutoAnual + '</td> </tr>' +
                 '<tr> <td>VALOR LÍQUIDO ANUAL (R$)</td> <td>' + conversaoValorLiquidoPorAno + '</td> </tr>' +
